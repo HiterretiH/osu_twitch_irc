fdc53042be8b13482b3cf9a2a465bdc3845c6ad1
HiterretiH <47186981+HiterretiH@users.noreply.github.com>
2021-07-26 02:43:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 osuirc_name = ""
 osuirc_password = ""  # https://osu.ppy.sh/p/irc
 osuirc_destination = ""  # user nickname
-osuapi_id = 0000  # oauth application id, create one here https://osu.ppy.sh/home/account/edit#new-oauth-applicationt
+osuapi_id = 0000  # oauth application id, create one here https://osu.ppy.sh/home/account/edit#new-oauth-application
 osuapi_secret = ""  # oauth application secret
 twitch_name = ""
 twitch_password = ""  # https://twitchapps.com/tmi/
